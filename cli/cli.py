4a041b78eeca05e94a109d99a2ec4978f5b27731
JannisKohle <Jannis.Kohle@icloud.com>
2020-09-07 14:39:20 +0200

    bit of work on cli

--- cli/cli.py
+++ cli/cli.py
@@ -3,11 +3,12 @@ import sys
 
 args = sys.argv[1:]
 
+usage = "Type 'pimessages help' for usage"
+
 if args[0] == "signup":
     args = args[1:]
-    usage = "Type 'pimessages help' for usage"
 
-    if len(args) == 0: # no more args
+    if len(args) == 0:  # no more args
         username = input("Username: ")
         password = input("Password: ")
 
@@ -45,9 +46,59 @@ if args[0] == "signup":
         print(usage)
 
 elif args[0] == "login":
-    pass
+    args = args[1:]
+
+    if len(args) == 0:  # no more args
+        username = input("Username: ")
+        password = input("Password: ")
+
+    elif len(args) == 2:
+        if args[0] in ["-un", "--username"]:
+            username = args[1]
+            password = input("Password: ")
+
+        elif args[0] in ["-pw", "--password"]:
+            password = args[1]
+            username = input("Username: ")
+
+        else:
+            print(usage)
+
+    elif len(args) == 4:
+        if args[0] in ["-un", "--username"]:
+            if args[2] in ["-pw", "--password"]:
+                username = args[1]
+                password = args[3]
+
+            else:
+                print(usage)
+
+        elif args[0] in ["-pw", "--password"]:
+            if args[2] in ["-un", "--username"]:
+                username = args[3]
+                password = args[1]
+
+            else:
+                print(usage)
+        else:
+            print(usage)
+    else:
+        print(usage)
+
 elif args[0] == "send":
-    pass
+    args = args[1:]
+
+    if len(args) == 1:
+        message = input("Message: ")
+
+    elif len(args) == 3:
+        if args[1] == "-m" or args[1] == "--message":
+            pass
+        else:
+            print(usage)
+    else:
+        print(usage)
+
 elif args[0] == "chat":
     pass
 elif args[0] == "ignore":
